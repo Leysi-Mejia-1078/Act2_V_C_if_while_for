ca429a31760ff06f170274070570c71a8ef3450f
Leysi Mejia <164534001+Leysi-Mejia-1078@users.noreply.github.com>
2024-08-30 12:20:53 -0600

    Add files via upload

---
+++ misvariables.py
@@ -0,0 +1,7 @@
+# variables
+print("Mis variables")
+
+edad=16
+nombre="Leysi Mejia"
+print("Edad ", edad)
+print("Nombre ", nombre)
---
+++ parafor.py
@@ -0,0 +1,5 @@
+# For
+print("Ciclo for, de repeticion")
+misvecinas=["Lupita", "Ana", "Agripina"]
+for chicas in misvecinas:
+    print(chicas)
